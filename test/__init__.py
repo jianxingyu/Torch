bd3e435ac10f3e63d03825b7b0f94bc90945e8db
jianxingyu <852979268@qq.com>
2020-08-13 12:28:22 +0800

    编写Torch的GPU测试

---
+++ test/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/13 下午 12:25
+# @Author  : jianxingyu
+# @FileName: __init__.py.py
+# @Software: PyCharm
---
+++ test/torch_gpu_test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/13 下午 12:26
+# @Author  : jianxingyu
+# @FileName: torch_gpu_test.py
+# @Software: PyCharm
+
+import torch
+
+print("GPU is " , torch.cuda.is_available())
