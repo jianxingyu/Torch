6a862d37fd06c16c1db6c503e1ff7cc1a582a164
jianxingyu <852979268@qq.com>
2020-08-13 12:32:51 +0800

    修改

--- src/demo.py
+++ src/demo.py
@@ -2,16 +2,9 @@
 # @Time    : 2020/8/13 上午 11:33
 # @Author  : jianxingyu
 # @FileName: demo.py
-<<<<<<< HEAD
 # @Software: PyCharm
 
 
 if __name__ == '__main__':
     print("hello github")
     # helo
-
-
-
-=======
-# @Software: PyCharm
->>>>>>> origin/master
