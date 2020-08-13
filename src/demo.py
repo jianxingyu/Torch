cc67bdab06ec9116cd4571e868930f388c58eb07
jianxingyu <852979268@qq.com>
2020-08-13 11:33:58 +0800

    提交文件夹

---
+++ src/demo.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/13 上午 11:33
+# @Author  : jianxingyu
+# @FileName: demo.py
+# @Software: PyCharm
+
+
+if __name__ == '__main__':
+    print("hello github")
+
+
+
